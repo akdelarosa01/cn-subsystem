96473cd68709b12c38a3907d695f29629230f376
Kurt Dela Rosa <kurtcreampie@gmail.com>
2018-06-18 11:23:12 +0800

    Add committed files

--- app/Listeners/WHSCheckRequestFired.php
+++ app/Listeners/WHSCheckRequestFired.php
@@ -27,90 +27,36 @@ class WHSCheckRequestFired
      */
     public function handle(WHSCheckRequest $event)
     {
-        $db = DB::connection($event->conn)->table('tbl_request_detail')
-                ->select('transno','code','requestqty')
-                ->get();
+        $db = DB::connection($event->conn)->table('tbl_request_summary')
+                ->select('transno','status')
+                ->where('transno',$event->transno)
+                ->first();
 
         $status = "Closed";
-        foreach ($db as $key => $pmr) {
-            // $chck = DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance_details')
-            //         ->select('issuance_no','item',DB::raw("SUM(issued_qty_t) as issued_qty"))
-            //         ->groupBy('issuance_no','item')
-            //         ->where('request_no',$pmr->transno)
-            //         ->count();
-            
-            // if ($chck > 0) {
-            //     $whs = DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance_details')
-            //             ->select('issuance_no','item',DB::raw("SUM(issued_qty_t) as issued_qty"))
-            //             ->groupBy('issuance_no','item')
-            //             ->where('request_no',$pmr->transno)
-            //             ->first();
+        //foreach ($db as $key => $pmr) {
+        if ($db->status !== 'Cancelled') {
+            $requestqty = DB::connection($event->conn)->table('tbl_request_detail')
+                            ->where('transno',$db->transno)->sum('requestqty');
 
-            //     if ($pmr->requestqty == $whs->issued_qty) {
-            //         DB::connection($event->conn)->table('tbl_request_summary')
-            //             ->where('transno',$pmr->transno)
-            //             ->update(['status' => 'Closed']);
-            //     }
-            // }
-            $check = DB::connection($event->conn)->table('tbl_request_detail')
-                        ->select('transno',DB::raw('SUM(requestqty) as requestqty'),
-                                DB::raw('SUM(servedqty) as servedqty'))
-                        ->where('transno',$pmr->transno)
-                        ->groupBy('transno')
-                        ->get();
-            //if (isset($check[$key]->servedqty)) {
-                if ( ($check[0]->servedqty > 0) && ($check[0]->requestqty == $check[0]->servedqty) ) {
+            $servedqty = DB::connection($event->conn)->table('tbl_request_detail')
+                            ->where('transno',$db->transno)->sum('servedqty');
+
+                if ( ($servedqty > 0) && ($requestqty == $servedqty) ) {
                     $status = 'Closed';
                 }
 
-                if ( ($check[0]->servedqty < 1) ) {
+                if ( ($servedqty < 1) ) {
                     $status = 'Alert';
                 }
 
-                if (($check[0]->servedqty > 0) && ($check[0]->requestqty > $check[0]->servedqty)) {
+                if ( ($servedqty > 0) && ($requestqty > $servedqty)) {
                     $status = 'Serving';
                 }
 
                 DB::connection($event->conn)->table('tbl_request_summary')
-                    ->where('transno',$pmr->transno)
+                    ->where('transno',$db->transno)
                     ->update(['status' => $status]);
-
-                DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance')
-                        ->where('request_no',$pmr->transno)
-                        ->update(['status' => $status]);
-            //}
             
         }
-
-        // $db = DB::connection($event->conn)->table('tbl_request_detail')
-        //         ->select('transno','code','requestqty')
-        //         ->whereIn('status',['Alert','Serving'])
-        //         ->get();
-                
-        // foreach ($db as $key => $pmr) {
-        //     $chck = DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance_details')
-        //             ->select('issuance_no','item',DB::raw("SUM(issued_qty_t) as issued_qty"))
-        //             ->groupBy('issuance_no','item')
-        //             ->where('request_no',$pmr->transno)
-        //             ->count();
-            
-        //     if ($chck > 0) {
-        //         $whs = DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance_details')
-        //                 ->select('issuance_no','item',DB::raw("SUM(issued_qty_t) as issued_qty"))
-        //                 ->groupBy('issuance_no','item')
-        //                 ->where('request_no',$pmr->transno)
-        //                 ->first();
-        //         if ($pmr->requestqty == $whs->issued_qty) {
-        //             DB::connection($event->conn)->table('tbl_request_summary')
-        //                 ->where('transno',$pmr->transno)
-        //                 ->update(['status' => 'Closed']);
-
-        //             DB::connection($event->conn)->table('tbl_wbs_warehouse_mat_issuance')
-        //                 ->where('request_no',$pmr->transno)
-        //                 ->update(['status' => 'Closed']);
-        //         }
-        //     }
-            
-        // }
     }
 }
