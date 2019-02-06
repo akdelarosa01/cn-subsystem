86cbcaa053825c2814dc3cacfb82d3f05fcb9a78
dennis abenion <abeniondennis11@gmail.com>
2019-02-06 15:57:25 +0800

    fix MRA table

--- public/assets/global/scripts/mra.js
+++ public/assets/global/scripts/mra.js
@@ -100,7 +100,6 @@ function loadMRA()
 						'<td>'+Number(x.TtlCompleted).toLocaleString('en')+'</td>'+
 						'<td>'+Number(x.ReqToComplete).toLocaleString('en')+'</td>'+
 						'<td>'+Number(x.WHSE100).toLocaleString('en')+'</td>'+
-						'<td>'+Number(x.WHSE102).toLocaleString('en')+'</td>'+
 						'<td>'+Number(x.WHSE_NON).toLocaleString('en')+'</td>'+
 						'<td>'+Number(x.ASSY100).toLocaleString('en')+'</td>'+
 						'<td>'+Number(x.ASSY102).toLocaleString('en')+'</td>'+
